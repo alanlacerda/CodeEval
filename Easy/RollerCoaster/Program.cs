a5363d063f5a645edd55ee503cdd6bf5b413c355
Alan Lacerda <alan.lcda@gmail.com>
2015-06-06 01:02:14 -0300

    Roller Coaster first try.

---
+++ Easy/RollerCoaster/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace RollerCoaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in File.ReadLines(args[0]))
+            {
+                if (line.Length == 0)
+                {
+                    continue;
+                }
+
+                string[] words = line.Split(' ');
+
+                bool wasUpper = false;
+                int wordLimit = words.Length;
+
+                for (int wordIndex = 0; wordIndex < wordLimit; ++wordIndex)
+                {
+                    string word = words[wordIndex];
+                    int charlimit = word.Length;
+
+                    for (int charIndex = 0; charIndex < charlimit; ++charIndex)
+                    {
+                        char character = word[charIndex];
+
+                        if (char.IsLetter(character))
+                        {
+                            if (wasUpper)
+                            {
+                                character = char.ToLower(character);
+                                wasUpper = false;
+                            }
+                            else
+                            {
+                                character = char.ToUpper(character);
+                                wasUpper = true;
+                            }
+                        }
+
+                        Console.Write(character);
+                    }
+
+                    Console.Write(" ");
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
