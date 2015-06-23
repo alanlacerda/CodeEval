21ce93abd5d3a9f20186a9011cd31bbc45153b4b
Alan Lacerda <alan.lcda@gmail.com>
2015-06-22 23:44:09 -0300

    Minor optimization for Number Pairs.

--- Medium/NumberPairs/Program.cs
+++ Medium/NumberPairs/Program.cs
@@ -6,13 +6,13 @@ namespace NumberPairs
     {
         static void Main(string[] args)
         {
-            foreach (string line in System.IO.File.ReadLines(args[0]))
+            foreach (var line in System.IO.File.ReadLines(args[0]))
             {
                 if (line.Length == 0)
                     continue;
-                
+
                 var numbers = Array.ConvertAll(line.Substring(0, line.IndexOf(';')).Split(','), int.Parse);
-                int expectedSum = int.Parse(line.Substring(line.IndexOf(';') + 1));
+                int sum = int.Parse(line.Substring(line.IndexOf(';') + 1));
                 int limit = numbers.Length - 1;
 
                 string pairs = "NULL";
@@ -20,21 +20,25 @@ namespace NumberPairs
                 for (int current = 0; current < limit - 1; ++current)
                 {
                     int left = numbers[current];
-                    int wanted = expectedSum - left;
 
                     for (int last = limit; last > current; --last)
                     {
                         int right = numbers[last];
 
-                        if (right == wanted)
-                        {
-                            if (pairs == "NULL")
-                                pairs = left + "," + right;
-                            else
-                                pairs = pairs + ";" + left + "," + right;
-                            
+                        if (right > sum - left)
+                            continue;
+
+                        if (right < sum - left)
                             break;
-                        }
+
+                        if (pairs == "NULL")
+                            pairs = left + "," + right;
+                        else
+                            pairs = pairs + ";" + left + "," + right;
+
+                        --limit;
+
+                        break;
                     }
                 }
 
