19ce1b15b7439676988ddc8b8d29cb78bbbe4c7b
Alan Lacerda <alan.lcda@gmail.com>
2015-06-22 21:09:56 -0300

    Mth To Last Element.

---
+++ Medium/MthToLastElement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MthToLastElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var line in System.IO.File.ReadLines(args[0]))
+            {
+                var elements = line.Split(' ');
+                int last = elements.GetUpperBound(0);
+                int index = int.Parse(elements[last]);
+
+                if (index > last)
+                    continue;
+
+                Console.WriteLine(elements[last - index]);
+            }
+        }
+    }
+}
