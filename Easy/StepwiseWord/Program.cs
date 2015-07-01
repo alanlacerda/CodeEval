44f51d3aaaefd319493e0f3a2efa7b213bc860ee
Alan Lacerda <alan.lcda@gmail.com>
2015-07-01 00:00:23 -0300

    Stepwise Words.

---
+++ Easy/StepwiseWord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StepwiseWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in System.IO.File.ReadLines(args[0]))
+            {
+                string longest = "";
+
+                foreach (string word in line.Split(' '))
+                {
+                    if (word.Length > longest.Length)
+                        longest = word;
+                }
+
+                for (int i = 0; i < longest.Length; ++i)
+                {
+                    Console.Write(new string('*', i) + longest[i] + ' ');
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+        }
+    }
+}
