24fd340fd31ca3c64af81003b3d610305c60c7cf
Alan Lacerda <alan.lcda@gmail.com>
2015-07-04 16:14:03 -0300

    Strings and arrows

--- Easy/RollerCoaster/Program.cs
+++ Easy/RollerCoaster/Program.cs
@@ -8,25 +8,14 @@ namespace RollerCoaster
         {
             foreach (string line in System.IO.File.ReadLines(args[0]))
             {
-                bool wasUpper = false;
-                    
-                foreach (string word in line.Split(' '))
+                bool isUpper = false;
+
+                foreach (char character in line)
                 {
-                    foreach (char character in word)
-                    {
-                        if (char.IsLetter(character))
-                        {
-                            Console.Write(wasUpper ? char.ToLower(character) : char.ToUpper(character));
-
-                            wasUpper = !wasUpper;
-                        }
-                        else
-                        {
-                            Console.Write(character);    
-                        }
-                    }
-
-                    Console.Write(" ");
+                    if (char.IsLetter(character))
+                        Console.Write((isUpper = !isUpper) ? char.ToUpper(character) : char.ToLower(character));
+                    else
+                        Console.Write(character);
                 }
 
                 Console.Write(Environment.NewLine);
---
+++ Easy/StringsAndArrows/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringsAndArrows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string rightArrow = ">>-->";
+            const string leftArrow = "<--<<";
+
+            foreach (var line in System.IO.File.ReadLines(args[0]))
+            {
+                if (line.Length < 5)
+                    continue;
+
+                int count = ((line.Length - line.Replace(rightArrow, "").Length) / 5) +
+                             ((line.Length - line.Replace(leftArrow, "").Length) / 5);
+                
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
