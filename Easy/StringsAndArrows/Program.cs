f10118327d426ab5511443b2a5386029c06a62f6
Alan Lacerda <alan.lcda@gmail.com>
2015-07-04 23:00:52 -0300

    Improvement in Strings and Arrows.

--- Easy/StringsAndArrows/Program.cs
+++ Easy/StringsAndArrows/Program.cs
@@ -4,19 +4,20 @@ namespace StringsAndArrows
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            const string rightArrow = ">>-->";
-            const string leftArrow = "<--<<";
-
             foreach (var line in System.IO.File.ReadLines(args[0]))
             {
-                if (line.Length < 5)
-                    continue;
+                int count = 0;
 
-                int count = ((line.Length - line.Replace(rightArrow, "").Length) / 5) +
-                             ((line.Length - line.Replace(leftArrow, "").Length) / 5);
-                
+                for (int i = 0; i + 4 < line.Length; ++i)
+                {
+                    string chunk = line.Substring(i, 5);
+
+                    if (chunk == ">>-->" || chunk == "<--<<")
+                        ++count;
+                }
+
                 Console.WriteLine(count);
             }
         }
